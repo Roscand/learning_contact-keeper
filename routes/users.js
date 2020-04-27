a90a54218eba132dc261c409038c009f50b0f732
RoscandDesign <roscanddesign@gmail.com>
2020-04-27 18:20:56 +0400

    Registration & Password Hashing

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 // dependencies
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 // router
@@ -15,12 +16,34 @@ router.post('/', [
     check('name', 'Please enter your name.').not().isEmpty(),
     check('email', 'Please enter a valid email address.').isEmail(),
     check('password', 'Please enter a password 6 or more characters long.').isLength({min:6})
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     } else {
-        res.send("Validation passed.");
+        // get the passed values
+        const { name, email, password } = req.body;
+        try {
+            // check if the user already exists
+            let user = await User.findOne({ email });
+            if (user) { return res.status(400).json({msg: 'User already exists.'}) };
+            // create a new User object
+            user = new User({
+                name, 
+                email, 
+                password
+            });
+            // encrypt the password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            // save the user to the db
+            await user.save();
+            // respond with the success message
+            res.send('User is successfully added to the database.');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        };
     };
 });
 
