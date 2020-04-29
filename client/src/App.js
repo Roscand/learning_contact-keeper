9024194a3289d938021b063cca71c1d422b445de
RoscandDesign <roscanddesign@gmail.com>
2020-04-29 21:13:56 +0400

    Context API: Auth State started

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+// context //
+import AuthState from './context/auth/AuthState';
+import ContactState from './context/contact/ContactState';
+
 // components //
 import Navbar from './components/layout/Navbar';
 
@@ -10,25 +14,24 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
-// context //
-import ContactState from './context/contact/ContactState';
-
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
-}
+};
 
 export default App;
