893b94a1920a7cc2dd2a2d54cdc83b3567777d07
RoscandDesign <roscanddesign@gmail.com>
2020-05-01 16:25:24 +0400

    PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import setAuthToken from './utils/setAuthToken';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 
 // contexts //
@@ -32,7 +33,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
